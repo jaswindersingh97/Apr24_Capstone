e4a49e6fa1181d4254a2978ef2ad7f44d7d8e2d5
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-07-30 13:46:39 +0530

    Started working on Movies page, added the gernres
    need to find the correct api for the movies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import GenrePage from "./pages/GenrePage";
 import CarouselPage from "./pages/CarouselPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import DashboardPage from "./pages/DashboardPage";
+import MoviesPage from "./pages/MoviesPage";
 
 function App() {
 	return (
@@ -14,7 +15,7 @@ function App() {
 				<Route path="/genres" element={<GenrePage />} />
 				<Route path="/carousel" element={<CarouselPage />} />
 				<Route path="/dashboard" element={<DashboardPage />} />
-				<Route path="/movies" element={<RegisterPage />} />
+				<Route path="/movies" element={<MoviesPage />} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</BrowserRouter>
---
+++ client/src/pages/MoviesPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './MoviesPage.module.css'
+import avatar from './../assets/avtar2.png'
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+function MoviesPage() {
+    const {selectedGenres}=useContext(AppContext)
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <p>Super app</p>
+        <img src={avatar}/>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.subheading}>
+        <p>Entertainment according to your choice</p>
+        </div>
+        <div className={styles.genres}>
+            {selectedGenres?.slice(0, 4)?.map((genre, index) => (
+			    <div key={index} className={styles.genre}>
+				    <p>{genre}</p>
+                    
+                </div>
+					))}
+				</div>
+      </div>
+    </div>
+  )
+}
+
+export default MoviesPage
